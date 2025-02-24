909edd0c4aecfd88403b4705e65aacf866565e9a
Yugender025 <pentapuriyugender@gmail.com>
2025-02-24 18:23:33 +0530

    final delpoyed code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 document.querySelector(".knw-btn").addEventListener("click", function () {
-  return "error occured";
   document
     .querySelector(".abtcmpy_innerContainer")
     .scrollIntoView({ behavior: "smooth" });
